235aa9eb768b9967cf353f9179d7f59516be66da
Reda BELHAJ <58605073+Reda-BELHAJ@users.noreply.github.com>
2021-06-28 21:15:26 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,163 +1,75 @@
-import discord
-from discord.ext import commands
-from discord_slash import SlashCommand, SlashContext
-from discord_slash.utils.manage_commands import create_choice, create_option
 import os
-import random
-import utility
-from keep_alive import keep_alive
+import requests
+import datetime
+from yahoo_fin import stock_info as si
 
-client     = commands.Bot(command_prefix="/")
-slash      = SlashCommand(client, sync_commands=True)
+API_KEY     = os.environ['API_KEY']
+LIMIT_NEWS  = '100'
 
-@slash.slash(
-  name = "invite",
-  description = "This command sends you the invite link for the bot."
-)
-async def _invite(ctx:SlashContext):
-  embed = discord.Embed(color=discord.Color.from_rgb(3, 252, 144))
-  embed.description = "The Invite URL for Me! is : [Click Here](" + utility.Invite_URL+")" 
-  await ctx.send(embed=embed)
+CLIENT_ID   = os.getenv('CLIENT_ID')
+PERM        = '8'
 
-@slash.slash(
-  name = "Ping",
-  description = "This command returns the ping."
-)
-async def _Ping(ctx:SlashContext):
-    embed = discord.Embed(color=discord.Color.from_rgb(3, 252, 144))
-    embed.description = f':hourglass: {round(client.latency * 1000)}ms'
-    await ctx.send(embed=embed)
+Invite_URL = f'https://discord.com/api/oauth2/authorize?client_id={CLIENT_ID}&permissions={PERM}&scope=bot%20applications.commands'
 
-@slash.slash(
-  name = "Help",
-  description = "This command returns this embed."
-)
-async def _Help(ctx:SlashContext):
-    embed = discord.Embed(color=discord.Color.from_rgb(20,52,116))
-    embed.title = "Help"
-    embed.description = "Here are the commands that you can use:"
-    embed.set_thumbnail(url=client.user.avatar_url)
+def getPrice(ticker):
+  price = '0'
+  try:
+    price = si.get_live_price(ticker)
+  except:
+    pass
+  return price
 
-    embed.add_field(name="/Ping", value="This command returns the ping.", inline=True)
-    embed.add_field(name="/News [Ticker]", value="This command returns the news of the ticker specified.", inline=True)
-    embed.add_field(name="/Help", value="This command returns this message.", inline=True)
-    embed.add_field(name="/Invite", value="This command sends you the invite link for the bot.", inline=True)
-    embed.add_field(name="/Price [Ticker]", value="This command returns the current price of the ticker specified.", inline=True)
-    embed.add_field(name="/Detail [Ticker]", value="This command returns the details of the ticker specified.", inline=True)
+def getDetails(ticker):
+    Detail_URL = f'https://api.polygon.io/v1/meta/symbols/{ticker}/company?&apiKey={API_KEY}'
+    Data       = requests.get(Detail_URL).json()
 
-    embed.set_footer(text="Information requested by: " + ctx.author.name)
+    Logo          = ''
+    URL           = ''
+    Description   = ''
+    Name          = ''
+    Industry      = ''
+    Symbol        = ''
 
-    await ctx.send(embed=embed)
-
-@slash.slash(
-  name = "Price",
-  description = "This command returns the current price of the ticker specified.",
-  options = [
-    create_option(
-      name = "ticker",
-      description = "Choose a Ticker!",
-      required = True,
-      option_type = 3
-    )
-  ]
-)
-async def _Price(ctx:SlashContext, ticker:str):
-  price = utility.getPrice(ticker)
-  if price != '0':
-    embed = discord.Embed(color=discord.Color.from_rgb(20,52,116))
-
-    embed.title = ticker
-    embed.add_field(name="Price", value=str(price)[0:10], inline=True)
-    embed.set_footer(text="Information requested by: " + ctx.author.name)
-
-    await ctx.send(embed=embed)
-  else:
-    embed = discord.Embed(color=discord.Color.from_rgb(255,0,0))
-    embed.title = "Ticker Not Found"
-    embed.add_field(name="Price Command Usage", value="/Price [Ticker]", inline=True)
-    embed.add_field(name="Ticker Validation", value="Make sure that the ticker is verifed and in uppercase.", inline=True)
-    embed.set_footer(text="Information requested by: " + ctx.author.name)
-    await ctx.send(embed=embed)
-
-@slash.slash(
-  name = "Detail",
-  description = "This command returns the details of the ticker specified.",
-  options = [
-    create_option(
-      name = "ticker",
-      description = "Choose a Ticker!",
-      required = True,
-      option_type = 3,
-    )
-  ]
-)
-async def _Detail(ctx:SlashContext, ticker:str):
-  Logo, URL, Description, Name, Industry, Symbol = utility.getDetails(ticker)
-
-  if Description != '':
-
-    embed = discord.Embed(color=discord.Color.from_rgb(20,52,116))
-    embed.title = Name
-    embed.description = Description + " [Click Here](" + URL + ")."
-    embed.set_thumbnail(url=Logo)
-
-    embed.add_field(name="Industry", value=Industry, inline=True)
-    embed.add_field(name="Symbol", value=Symbol, inline=True)
-
-    embed.set_footer(text="Information requested by: " + ctx.author.name)
-
-    await ctx.send(embed=embed)
-  
-  else:
-    embed = discord.Embed(color=discord.Color.from_rgb(255,0,0))
-    embed.title = "Ticker Not Found"
-    embed.add_field(name="Detail Command Usage", value="/Detail [Ticker]", inline=True)
-    embed.add_field(name="Ticker Validation", value="Make sure that the ticker is verifed and in uppercase.", inline=True)
-    embed.set_footer(text="Information requested by: " + ctx.author.name)
-
-    await ctx.send(embed=embed)
-
-@slash.slash(
-  name = "News",
-  description = "This command returns the News of the ticker specified.",
-  options = [
-    create_option(
-      name = "ticker",
-      description = "Choose a Ticker!",
-      required = True,
-      option_type = 3
-    )
-  ]
-)
-async def _News(ctx:SlashContext, ticker:str):
-  description, url, title, image = utility.getNews(ticker)
-
-  if len(description) != 0:
-    randomNumber = random.randint(0, int(len(description) - 1))
+    try:
+      Logo          = Data['logo']
+      URL           = Data['url']
+      Description   = Data['description']
+      Name          = Data['name']
+      Industry      = Data['industry']
+      Symbol        = Data['symbol']
+    except:
+      pass
     
-    news = description[randomNumber] + " [Click Here](" + url[randomNumber] + ")."
+    return Logo, URL, Description, Name, Industry, Symbol
 
-    embed = discord.Embed(color=discord.Color.from_rgb(20,52,116))
-    embed.title = title[randomNumber]
-    embed.description = news
-    embed.set_image(url=image[randomNumber])
-    embed.set_footer(text="Information requested by: " + ctx.author.name)
+def getNews(ticker):
+    today = datetime.datetime.utcnow().date()
 
-    await ctx.send(embed=embed)
+    yesterday = today - datetime.timedelta(days=3)
 
-  else:
-      embed = discord.Embed(color=discord.Color.from_rgb(255,0,0))
-      embed.title = "Ticker Not Found"
-      embed.add_field(name="News Command Usage", value="/News [Ticker]", inline=True)
-      embed.add_field(name="Ticker Validation", value="Make sure that the ticker is verifed and in uppercase.", inline=True)
-      embed.set_footer(text="Information requested by: " + ctx.author.name)
+    API_URL_NEWS     = f'https://api.polygon.io/v2/reference/news?limit={LIMIT_NEWS}&order=descending&sort=published_utc&ticker={ticker}&published_utc.gte={yesterday}&apiKey={API_KEY}'
+    Data = requests.get(API_URL_NEWS).json()
 
-      await ctx.send(embed=embed)
+    List_Description   = []
+    List_URL_Article   = []
+    List_Title         = []
+    List_Image_URL     = []
 
+    for i in range(int(LIMIT_NEWS)):
+        try:
+            newsDes     = Data['results'][i]['description']
+            newsURL     = Data['results'][i]['article_url']
+            newsTitle   = Data['results'][i]['title']
+            image       = Data['results'][i]['image_url']
 
-@client.event
-async def on_ready():
-    print("We have logged in as {0.user}".format(client))
+            List_Description.append(newsDes)
+            List_URL_Article.append(newsURL)
+            List_Title.append(newsTitle)
+            List_Image_URL.append(image)
+        except:
+            pass
 
-keep_alive()
-client.run(os.getenv('TOKEN'))
+    return List_Description, List_URL_Article, List_Title, List_Image_URL
+
+def getChart(ticker):
+  pass
